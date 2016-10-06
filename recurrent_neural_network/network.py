fab2873b0a0d7e6be8308e2630a3125bba4842a1
William Wolf <wwolf@shopkeep.com>
2016-10-06 15:43:16 +0100

    add recurrent neural network training data builder, training data object, and tests
    * test that builder creates the correct X_train and y_train datasets using a sufficiently
    * large vocabulary whereby no words are removed
    * test that builder creates the correct X_train and y_train datasets using a small vocabulary
    * whereby some words are removed

---
+++ recurrent_neural_network/network.py
@@ -0,0 +1,1 @@
+# you should pass in a _feed_forward function from inside of your network to the optimization routine, so you don't repeat yourself.
---
+++ recurrent_neural_network/training_data.py
@@ -0,0 +1,72 @@
+from collections import namedtuple
+import itertools
+import nltk
+
+
+class WordLevelRNNTrainingDataBuilder:
+
+    UNKNOWN_TOKEN = 'UNKNOWN_TOKEN'
+    NUMBER_OF_WORDS_TO_ADD_IN_MANUALLY = len(['UNKNOWN_TOKEN', 'SENTENCE_START', 'SENTENCE_END'])
+    WORD_COUNT_ITEM = namedtuple('WordCountItem', ['word', 'count'])
+
+    @classmethod
+    def build(cls, corpus, vocabulary_size):
+        tokenized_corpus = cls._tokenize_corpus_into_list_of_tokenized_sentences(corpus)
+        tokenized_corpus = cls._remove_uncommon_words(tokenized_corpus, vocabulary_size)
+        tokenized_corpus = cls._append_sentence_start_and_end_tokens(tokenized_corpus)
+        return RNNTrainingData(tokenized_corpus=tokenized_corpus)
+
+    @classmethod
+    def _tokenize_corpus_into_list_of_tokenized_sentences(cls, corpus):
+        tokenized_corpus = nltk.sent_tokenize(corpus)
+        tokenized_corpus = [cls._clean_sentence(sentence) for sentence in tokenized_corpus]
+        return [nltk.word_tokenize(sentence) for sentence in tokenized_corpus]
+
+    @classmethod
+    def _remove_uncommon_words(cls, tokenized_corpus, vocabulary_size):
+        word_count = nltk.FreqDist( itertools.chain(*tokenized_corpus) )
+        word_count = [cls.WORD_COUNT_ITEM(word=word, count=count) for word, count in word_count.items()]
+        word_count = sorted(word_count, key=lambda item: (item.count, item.word), reverse=True)
+        most_common_words = [word_count_item.word for word_count_item in word_count[:vocabulary_size - \
+            cls.NUMBER_OF_WORDS_TO_ADD_IN_MANUALLY + 1]]
+
+        tokenized_corpus = [
+            [word if word in most_common_words else cls.UNKNOWN_TOKEN for word in sentence]\
+            for sentence in tokenized_corpus
+        ]
+        return tokenized_corpus
+
+    @staticmethod
+    def _clean_sentence(sentence):
+        return sentence.lower()
+
+    @staticmethod
+    def _append_sentence_start_and_end_tokens(tokenized_corpus):
+        return [['SENTENCE_START'] + sentence + ['SENTENCE_END'] for sentence in tokenized_corpus]
+
+
+class RNNTrainingData:
+
+    def __init__(self, tokenized_corpus):
+        self.training_data_as_tokens = tokenized_corpus
+        self.token_to_index_lookup = self._compose_token_to_index_lookup(tokenized_corpus)
+        self.training_data_as_indices = self._indexify_tokenized_corpus()
+        self.X_train = self._compose_X_train()
+        self.y_train = self._compose_y_train()
+
+    def _indexify_tokenized_corpus(self):
+        return [[self.token_to_index_lookup[token] for token in sentence] \
+                for sentence in self.training_data_as_tokens]
+
+    def _compose_X_train(self):
+        return [training_instance[:-1] for training_instance in self.training_data_as_indices]
+
+    def _compose_y_train(self):
+        return [training_instance[1:] for training_instance in self.training_data_as_indices]
+
+    @staticmethod
+    def _compose_token_to_index_lookup(tokenized_corpus):
+        unique_tokens = set( itertools.chain(*tokenized_corpus) )
+        unique_tokens = list(unique_tokens)
+        unique_tokens.sort()
+        return {token: index for index, token in enumerate(unique_tokens)}
