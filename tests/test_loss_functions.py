962098331baadb100a5e2172be4e3f3c61d4408f
William Wolf <wwolf@shopkeep.com>
2016-10-17 09:18:33 +0100

    change CrossEntropyLoss to BinaryCrossEntropyLoss

--- tests/test_loss_functions.py
+++ tests/test_loss_functions.py
@@ -2,7 +2,7 @@ import unittest
 
 from base.loss_function import BaseLossFunction
 from vanilla_neural_nets.neural_network.loss_function import MeanSquaredError
-from vanilla_neural_nets.neural_network.loss_function import CrossEntropyLoss
+from vanilla_neural_nets.neural_network.loss_function import BinaryCrossEntropyLoss
 from vanilla_neural_nets.recurrent_neural_network.loss_function import CrossEntropyLoss as RNNCrossEntropyLoss
 
 
@@ -13,7 +13,7 @@ class TestLossFunctions(unittest.TestCase):
         self.assertEqual(mean_squared_error.__class__.__base__, BaseLossFunction)
 
     def test_cross_entropy_base_class_is_base_loss_function(self):
-        cross_entropy_loss = CrossEntropyLoss()
+        cross_entropy_loss = BinaryCrossEntropyLoss()
         self.assertEqual(cross_entropy_loss.__class__.__base__, BaseLossFunction)
 
     def test_rnn_cross_entropy_base_class_is_base_loss_function(self):
