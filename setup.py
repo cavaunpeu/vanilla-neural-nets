b2a3ab6347c560b699850fcf53c465aaaee70331
William Wolf <wwolf@shopkeep.com>
2017-04-01 18:25:39 +0100

    add setup files, license to ready for pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'vanilla-neural-nets',
+  packages = ['vanilla-neural-nets'],
+  version = '0.1',
+  description = 'A straightforward and highly readable implementation of vanilla neural nets',
+  author = 'William Wolf',
+  author_email = 'williamabrwolf@gmail.com',
+  url = 'https://github.com/cavaunpeu/vanilla-neural-nets',
+  download_url = 'https://github.com/cavaunpeu/vanilla-neural-nets/archive/0.1.tar.gz',
+  keywords = ['neural networks', 'data science'],
+  classifiers = [],
+)
